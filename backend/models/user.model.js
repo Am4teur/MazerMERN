5ec0e2f06ce9ab7378a16083136ae37420830c48
Am4teur <daniel.7c.n12@gmail.com>
2020-09-22 23:58:09 +0100

    Added email and hashedPassword to User model
    [Backend | Authentication]

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,6 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
   username: {
     type: String,
     required: true,
