333ae98dd3025c76061f27dbba4bff511ef0f2ad
am4teur <daniel.7c.n12@gmail.com>
2020-11-16 21:34:28 +0000

    Improved the home page

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -18,10 +18,12 @@ const UserInfo = (props: UserInfoProps) => {
       <label htmlFor="icon" style={{color: "white"}}>Icon: <IconComponent iconName={props.icon} size={16}/></label>
       <br/>
       <label htmlFor="id" style={{color: "white"}}>id: {userData.user.id}</label>
+      {/*
       <br/>
       <label htmlFor="x" style={{color: "white"}}>x: {userData.user.x}</label>
       <br/>
       <label htmlFor="y" style={{color: "white"}}>y: {userData.user.y}</label>
+      */}
     </div>
   );
 }
