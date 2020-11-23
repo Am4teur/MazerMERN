1987fcba44d0453253ad1ba3ca3e4ac30b0da8cb
am4teur <daniel.7c.n12@gmail.com>
2020-11-23 07:34:05 +0000

    Removed mazes from User object (attribute and interface)

--- frontend/src/objects/User.tsx
+++ frontend/src/objects/User.tsx
@@ -1,30 +1,12 @@
 interface User {
   id: string,
   username: string,
-  mazes: mazeType,
 }
-
-export interface mazeType {
-  [mazeId: string] : any
-}
-
-/*export interface mazeType {
-  [mazeId: string] : {
-    users: any[],
-    name: string,
-    userCreater: string,
-    seed: number,
-    length: number,
-    width: number,
-    createdAt: string,
-  }
-}*/
 
 class User {
-  constructor(id: string="", username: string="", mazes: mazeType={}) {
+  constructor(id: string="", username: string="") {
     this.id = id;
     this.username = username;
-    this.mazes = mazes;
   }
 }
 
